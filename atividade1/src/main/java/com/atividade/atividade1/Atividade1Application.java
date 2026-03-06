7e807afab0eaf46160987d5b6336f694f4d8f3b4
Pablo Emanuel <169102342+PabloIsSpring@users.noreply.github.com>
2026-03-05 22:23:21 -0300

    Add files via upload

---
+++ atividade1/src/main/java/com/atividade/atividade1/Atividade1Application.java
@@ -0,0 +1,15 @@
+package com.atividade.atividade1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class Atividade1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade1Application.class, args);
+	}
+
+}
---
+++ atividade1/src/main/java/com/atividade/atividade1/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.atividade.atividade1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hello from Servlet!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
